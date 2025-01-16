35f73fd5adaa15718140f1f92ddd9b3b5ea4b88f
Jordan Marine <marine.jordan64@proton.me>
2025-01-15 20:26:23 -0600

    Fixed incorrect language variable name.

--- lua/terrortown/lang/en/ttt2_pronouns.lua
+++ lua/terrortown/lang/en/ttt2_pronouns.lua
@@ -6,4 +6,4 @@ L["pronouns_addon_info"] = "Pronouns"
 -- Server Addons > Pronouns > Anti-Ghost
 L["pronouns_settings_antighost"] = "Anti-Ghost"
 L["label_ttt2_pronouns_antighost"] = "Only Set Pronouns When Dead"
-l["help_ttt2_pronouns_antighost"] = "Prevents players from setting their pronouns when they are dead."
+L["help_ttt2_pronouns_antighost"] = "Prevents players from setting their pronouns when they are dead."
